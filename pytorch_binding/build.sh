63ce0f4472cec67f00e7d93e53b2b3c50f544891
Hui Zhang <zhtclz@foxmail.com>
2022-11-22 02:36:28 +0000

    build and notebook

--- pytorch_binding/build.sh
+++ pytorch_binding/build.sh
@@ -3,5 +3,8 @@
 # TMPDIR=$PWD pip install torch==1.11.0  -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 export CUDA_HOME="/usr/local/cuda"
+export C_INCLUDE_PATH="/usr/local/cuda/include"
+export CPLUS_INCLUDE_PATH="/usr/local/cuda/include"
 
-python setup.py install
+#python setup.py install
+pip install -e 
--- pytorch_binding/setup.py
+++ pytorch_binding/setup.py
@@ -36,7 +36,6 @@ if not os.path.exists(os.path.join(warp_rnnt_path, "libwarprnnt" + lib_ext)):
            " libwarprnnt.so (default is '../build')").format(warp_rnnt_path))
     sys.exit(1)
 include_dirs = [os.path.realpath('../include')]
-include_dirs += [os.environ["CUDA_HOME"] + '/include']
 
 setup(
     name='warprnnt_pytorch',
